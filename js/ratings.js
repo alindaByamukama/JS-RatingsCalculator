aad9c9c567746964f9555a9b6ec3e4a4aaf26eda
ASPB <alindabyamukama@gmail.com>
2021-11-18 22:13:03 +0300

    Completed module

--- js/ratings.js
+++ js/ratings.js
@@ -1,23 +1,28 @@
 function collect_ratings() {
     let ratings = 
     {
-        count: 0, 
-        sum: 0, 
-        average: 0
+        'count': 0, 
+        'sum': 0, 
+        'average': 0
     };
+
+    let rating = 0;
+    
     const elements = document.querySelectorAll('.rating');
-    let rating = elements.forEach(element => {
-         parseInt(element.id.replace('star', ''))
-         count += parseInt(element.value);
-         sum += parseInt(element.value * rating);
+    elements.forEach(element => {
+         rating = parseInt(element.id.replace('star', ''))
+         ratings.count += parseInt(element.value);
+         ratings.sum += parseInt(element.value) * rating;
     });
 
-    if (count != 0) { average = sum / count; }
+    if (ratings.count !== 0) { 
+        ratings.average = ratings.sum / ratings.count; 
+    }
 
     return ratings;
 };
 
-const ratings = document.addEventListener(change, function collect_ratings(){
-    document.querySelector('#average').setAttribute('value', ratings.average);
-    ratings.average.toFixed(2);
+ document.addEventListener('change', ()=>{
+    const ratings = collect_ratings();
+    document.querySelector('#average').value = ratings.average.toFixed(2);
 });
